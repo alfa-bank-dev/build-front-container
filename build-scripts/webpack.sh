3e9c194eb40b4cf35da133d072e97de4617afc8a
Nikita Korotkih <nkorotkikh@alfabank.ru>
2016-05-27 18:44:14 +0300

    fix(*): remove --production from npm install in webpack.sh

--- build-scripts/webpack.sh
+++ build-scripts/webpack.sh
@@ -3,6 +3,6 @@ rm -rf ./dist
 node --version
 npm --version
 
-npm i --unsafe-perm --production
+npm i --unsafe-perm
 APP_DEBUG=1 NODE_TARGET=standalone NODE_ENV=production npm run build
 APP_DEBUG=1 NODE_TARGET=standalone NODE_ENV=production npm run build-server
