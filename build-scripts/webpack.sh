a5975f6510502c702df18c1d3a5ae3bb2288eaf5
Nikita Korotkih <nkorotkikh@alfabank.ru>
2016-05-30 19:07:07 +0300

    minor fixes

--- build-scripts/webpack.sh
+++ build-scripts/webpack.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 rm -rf ./dist
 
-node --version
-npm --version
-
 npm i --unsafe-perm
+
 APP_DEBUG=1 NODE_TARGET=standalone NODE_ENV=production npm run build
 APP_DEBUG=1 NODE_TARGET=standalone NODE_ENV=production npm run build-server
